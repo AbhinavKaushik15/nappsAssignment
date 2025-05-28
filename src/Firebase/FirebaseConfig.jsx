558aeba9515e1ee33f17dae70f9e75ebcb577810
Abhinav Kaushik <abhinavsharmaas@gmail.com>
2025-05-28 15:25:14 +0530

    FirebaseConfig

--- src/Firebase/FirebaseConfig.jsx
+++ src/Firebase/FirebaseConfig.jsx
@@ -3,7 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
+const FirebaseConfig = {
   apiKey: "AIzaSyDetumm2NItwv0yfhCAACVxKwNaEJsgOlY",
   authDomain: "blogweb-405b0.firebaseapp.com",
   projectId: "blogweb-405b0",
@@ -12,7 +12,7 @@ const firebaseConfig = {
   appId: "1:402196643536:web:1d4cbd59c1ccc1a8a428d5",
 };
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(FirebaseConfig);
 export const fireDb = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
